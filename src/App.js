1d571f382a97bc347e267298ea53cd832068f6de
a0im <kzerozero1541@gmail.com>
2023-03-25 01:00:07 +0900

    [pkg-json,css]경로수정, 텍스트 커서잠금

--- src/App.js
+++ src/App.js
@@ -9,22 +9,7 @@ import Main from "./component/Main";
 import Footer from "./component/Footer";
 import My from "./component/My";
 
-/*
-  tailwind , reactRouter , redux // immer ??? 보류 //axios // react icons
-  grid 1240
-  컨텐츠 5개 // 스크롤 갯수 총 15개
 
-
-  제목
-  장르 공포 스릴러 로멘스 
-  종류 영화 만화 드라마 교양
-  시청점수
-  출시일
-  유료 무료 
- */
-  // style="bg-[#1b1b1b]"
-
-  // className="bg-[#1b1b1b]"
 const App = () => {
   const path = process.env.PUBLIC_URL;
 
@@ -39,9 +24,11 @@ const App = () => {
       dispatch(setContent(res.data))
     })
   },[])
+
+ 
   return (
     <div >
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter  basename={process.env.PUBLIC_URL}>
       <Header/>
       <Routes>
         <Route path="/" element={<Main/>}/>     
